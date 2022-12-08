3955b9b1327b7a0fd10e5afdc171fabe1e1d818c
Guille Vázquez <104373456+guillevazquez8@users.noreply.github.com>
2022-12-08 16:24:10 +0100

    Update lab302_activity2_DML.sql

--- lab302_activity2_DML.sql
+++ lab302_activity2_DML.sql
@@ -1,50 +1,50 @@
--- Activity 3 ---------------------
+-- Activity 3: get the total number of flights in the database -----------------------------------------------------------------------------------------------
 SELECT 
 	COUNT(*) 
 FROM 
 	flights;
 
--- 4 -----------------------------
+-- 4: get the average flight distance -----------------------------------------------------------------------------------------------------------------
 SELECT 
 	AVG(flightMileage) 
 FROM 
 	flights;
 
--- 5 -------------------------------
+-- 5: get the average number of seats ------------------------------------------------------------------------------------------------------------------
 SELECT
 	AVG(aircraftTotalSeats) 
 FROM 
 	aircrafts;
 
--- 6 --------------------------------
+-- 6: get the average number of miles flown by customers grouped by status ----------------------------------------------------------------------------
 SELECT 
 	customerStatus, AVG(customerTotalMileage)
 FROM 
 	customers
 GROUP BY customerStatus;
 
--- 7 ------------------------------------
+-- 7: get the maximum number of miles flown by customers grouped by status ------------------------------------------------------------------------------
 SELECT 
 	customerID, customerName, customerStatus, MAX(customerTotalMileage)
 FROM 
 	customers
 GROUP BY customerStatus;
 
--- 8 -----------------------------------
+-- 8: get the total number of aircraft with a name containing Boeing --------------------------------------------------------------------------------------
 SELECT 
 	COUNT(*) 
 FROM 
 	aircrafts
 WHERE aircraftModel LIKE 'Boeing%';
 
--- 9 ----------------------------------
+-- 9: find all flights with a distance between 300 and 2000 miles ----------------------------------------------------------------------------------------
 SELECT 
 	flightID, flightNumber, flightMileage 
 FROM 
 	flights
 WHERE flightMileage BETWEEN 300 AND 2000;
 
--- 10 --------------------------------
+-- 10: find the average flight distance booked grouped by customer status ---------------------------------------------------------------------------------
 SELECT
 	customerStatus, AVG(flightMileage)
 FROM
@@ -55,7 +55,7 @@ INNER JOIN
 	customers ON customers.customerID = customerDetails.customerID
 GROUP BY customerStatus;
 
--- 11 -----------------------------------
+-- 11: find the most often booked aircraft by gold status members ----------------------------------------------------------------------------------------
 SELECT
 	customerStatus, aircraftID,
     COUNT(aircraftID) AS aircraftsBooked
